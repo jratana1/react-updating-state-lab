abef9eb47951e842745b9a57ae9835c7bc9e1e6c
John Ratana <jratana1@gmail.com>
2021-02-25 14:31:09 -0500

    commit tree 8c1199400a17f503bc5bc0d0626189d1ce65fed9

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,73 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+    constructor() {
+      super();
+      // Define the initial state:
+      this.state = {
+        errors: [],
+        user: null,
+        settings: {
+            bitrate: 8,
+            video: {
+            resolution: '1080p'
+            }
+        }
+      };
+    }
+  
+    handleClickBit = (event) => {
+        // event.persist()
+        // this.setState(previousState => {
+        //     if (event.target.className === 'bitrate'){
+        //         return {
+        //             settings: {
+        //                 ...this.state.settings,
+        //                 bitrate: 12
+        //             }
+        //         }
+        //     } else if (event.target.className ==='resolution'){
+        //         return {
+        //             settings: {
+        //                 ...this.state.settings,
+        //                 resolution: '720p'
+        //             }
+        //         }
+        //     }
+        //   })
+        this.setState(previousState => {
+                return {
+                    settings: {
+                        ...this.state.settings,
+                        bitrate: 12
+                    }
+                }
+        })
+    }
+
+    handleClickResolution = () => {
+        this.setState({
+          settings: {
+            ...this.state.settings,
+            video: {
+              ...this.state.settings.video,
+              resolution: '720p'
+            }
+          }
+        });
+    }
+  
+    render() {
+      return (
+        <div>
+          <p>I have been clicked {this.state.timesClicked} times!</p>
+          <button onClick={this.handleClickBit} className='bitrate'>bitrate</button>
+          <button onClick={this.handleClickResolution} className='resolution'>resolution</button>
+
+        </div>
+      );
+    }
+  }
+  
+  export default YouTubeDebugger;
